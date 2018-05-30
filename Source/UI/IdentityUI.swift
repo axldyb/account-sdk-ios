f69b357397dee5283762b42ff2a230a61fb850b1
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-05-30 14:03:35 +0200

    Changed: do more logging work on a utility queue and add log levels

--- Source/UI/IdentityUI.swift
+++ Source/UI/IdentityUI.swift
@@ -185,7 +185,7 @@ public class IdentityUI {
 
             let msg = "Attempt to present updated terms while another Identity UI flow is already presented."
             assertionFailure(msg)
-            log(self, msg)
+            log(from: self, msg)
         }
 
         let navigationController = DismissableNavigationController {
@@ -353,7 +353,7 @@ extension IdentityUI: FlowCoordinator {
             guard case let .byRoute(route, _) = input else {
                 let msg = "Attempt to present a new Identity UI instance while another one is already presented."
                 assertionFailure(msg)
-                log(self, msg)
+                log(from: self, msg)
                 return
             }
 
